cd2bce235b5ec468c42cb08321c201eea1251378
khushiak44555 <163978136+khushiak44555@users.noreply.github.com>
2025-01-03 13:39:05 +0530

    Payment Verification issue successfull

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -36,7 +36,7 @@ export const verifyPayment = async (req, res) => {
     const body = razorpay_order_id + "|" + razorpay_payment_id;
 
     const expectedSignature = crypto
-        .createHmac('sha256', process.env.RAZORPAY_SECRET)
+        .createHmac('sha256', process.env.RZP_TEST)
         .update(body.toString())
         .digest('hex');
 
